eb40266de19c635051c4a4e0e049b05730098ecb
YongBum <dt10002@naver.com>
2025-05-19 14:30:08 +0900

    Init: 프로젝트 초기 설정

---
+++ src/main/java/ssafy/retrip/RetripApplication.java
@@ -0,0 +1,13 @@
+package ssafy.retrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetripApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RetripApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ssafy/retrip/RetripApplicationTests.java
@@ -0,0 +1,13 @@
+package ssafy.retrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetripApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
