0da233205e72f6e8c50fd06e6a2ab4a68f710cb4
Yongbum Kim <dt10002@naver.com>
2025-07-04 23:28:30 +0900

    Fix: 배포 환경 CORS 문제 해결 (#26)
    - CORS 문제를 해결하기 위해서 배포 환경에서의 도메인들을 addCorsMappings 메서드에 추가해줬습니다.
    - CORS 문제는 Nginx 측에서 해결하지 않고, 오로지 백엔드 서버에서 해결하도록 로직을 구현했습니다.
    
    issue #26

--- src/main/java/ssafy/retrip/config/WebConfig.java
+++ src/main/java/ssafy/retrip/config/WebConfig.java
@@ -5,26 +5,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private String FRONT_SERVER = "http://localhost:5267";
+  private String FRONTEND_LOCAL_SERVER = "http://127.0.0.1:5173";
+  private String BACKEND_LOCAL_SERVER = "http://127.0.0.1:8080";
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins(FRONT_SERVER)
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .exposedHeaders("Authorization")
-                .allowCredentials(true);
-    }
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/api/**")
+        .allowedOrigins(
+            "https://retrip.vercel.app",
+            "https://retrip.kr",
+            "https://www.retrip.kr",
+            "https://api.retrip.kr",
+            FRONTEND_LOCAL_SERVER,
+            BACKEND_LOCAL_SERVER
+        )
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .exposedHeaders("Authorization")
+        .allowCredentials(true);
+  }
 
-    @Bean
-    public RedirectStrategy redirectStrategy() {
-        return new DefaultRedirectStrategy();
-    }
+  @Bean
+  public RedirectStrategy redirectStrategy() {
+    return new DefaultRedirectStrategy();
+  }
 }
