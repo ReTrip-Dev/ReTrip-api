90809584b7a327edb30f5f0974d94b80c5ac36d9
Yongbum Kim <dt10002@naver.com>
2025-07-05 00:20:27 +0900

    Refactor: CD 워크플로우 수정 및 자동화 쉘 스크립트 수정 (#26)
    - CD 워크플로우를 수정합니다.
    - 기존 동시에 CI/CD 작업이 진행되는 것을 CI 작업이 성공적으로 끝난 다음에 CD 작업이 이루어지도록 수정했습니다.
    - .env 파일을 읽지 못하는 경우가 있어, deploy.sh 파일에 export 명령어와 sudo 명령어를 추가했습니다.
    
    issue #26

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,13 +5,12 @@
 # =================================================================
 
 echo "환경 설정을 시작합니다..."
-
-if [ -f .env ]; then
-    source .env
-else
+if [ ! -f .env ]; then
     echo ".env 파일이 없습니다. 스크립트를 중단합니다."
     exit 1
 fi
+
+export $(grep -v '^#' .env | xargs)
 
 MAIN_DOMAIN="retrip.kr"
 CERT_FILE_PATH="/etc/letsencrypt/live/$MAIN_DOMAIN/fullchain.pem"
@@ -26,15 +25,16 @@ else
     echo "Docker Compose를 찾을 수 없습니다! 스크립트를 중단합니다."
     exit 1
 fi
-echo "Docker Compose 명령어: $DOCKER_COMPOSE"
 
-mkdir -p $NGINX_CONF_DIR
+echo "Docker Compose 명령어: $DOCKER_COMPOSE"
+sudo mkdir -p $NGINX_CONF_DIR
+sudo mkdir -p ./data/certbot/www
 
 if [ ! -f "$CERT_FILE_PATH" ]; then
     echo "SSL 인증서가 존재하지 않습니다. Let's Encrypt 발급 절차를 시작합니다."
 
     echo "인증서 발급을 위해 임시 Nginx 설정을 적용합니다."
-    cp ./nginx-cert-setup.conf $NGINX_CONF_DIR/default.conf
+    sudo cp ./nginx-cert-setup.conf $NGINX_CONF_DIR/default.conf
 
     $DOCKER_COMPOSE up -d nginx
 
@@ -43,12 +43,9 @@ if [ ! -f "$CERT_FILE_PATH" ]; then
 
     echo "Certbot으로 SSL 인증서를 요청합니다..."
     $DOCKER_COMPOSE run --rm certbot certonly \
-      --webroot \
-      --webroot-path=/var/www/certbot \
+      --webroot --webroot-path=/var/www/certbot \
       -d retrip.kr -d www.retrip.kr -d api.retrip.kr \
-      --email $CERTBOT_EMAIL \
-      --agree-tos \
-      --no-eff-email
+      --email $CERTBOT_EMAIL --agree-tos --no-eff-email
 
     if [ $? -ne 0 ]; then
         echo "SSL 인증서 발급에 실패했습니다. Nginx 로그를 확인하세요."
@@ -57,15 +54,20 @@ if [ ! -f "$CERT_FILE_PATH" ]; then
     fi
     echo "SSL 인증서 발급 성공!"
     $DOCKER_COMPOSE down
-
 else
     echo "SSL 인증서가 이미 존재합니다. 발급 단계를 건너뜁니다."
 fi
 
-echo "최종 운영 설정을 적용하고 모든 서비스를 시작합니다."
-echo "운영용 Nginx 설정을 적용합니다."
-cp ./nginx-prod.conf $NGINX_CONF_DIR/default.conf
 
+echo "최종 운영 설정을 적용하고 모든 서비스를 시작합니다."
+
+echo "운영용 Nginx 설정을 적용합니다."
+sudo cp ./nginx-prod.conf $NGINX_CONF_DIR/default.conf
+
+echo "새로운 Docker 이미지를 pull 합니다"
+$DOCKER_COMPOSE pull retrip-app
+
+echo "모든 서비스를 시작/업데이트합니다..."
 $DOCKER_COMPOSE up -d
 
 echo "Nginx 설정을 리로드합니다..."
